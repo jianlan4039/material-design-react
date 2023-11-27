1c79e4ce0194d741715364bff9f4676036163b11
lan <lanjian1006@gmail.com>
2023-11-27 09:39:52 +0800

    1. alter to transpile typescript using babel instead of 'rollup-plugin-typescript2'
    2. build a separate css file

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,52 +1,53 @@
 // rollup.config.mjs
-import typescript from 'rollup-plugin-typescript2';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+// import typescript from 'rollup-plugin-typescript2';
+import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import sass from 'rollup-plugin-sass';
 
 export default {
-  input: 'src/index.ts', // Entry point of your TypeScript and React code
-  output: [
-    // {
-    //   file: 'dist/bundle.js',
-    //   format: 'cjs', // CommonJS format, but you can choose others like 'esm' for ES modules
-    //   sourcemap: true,
-    // },
-    {
-      file: 'dist/bundle.cjs.js',
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/bundle.esm.js',
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    // Resolve node_modules dependencies
-    nodeResolve({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
+    input: 'src/index.ts', // Entry point of your TypeScript and React code
+    output: [
+        // {
+        //   file: 'dist/bundle.js',
+        //   format: 'cjs', // CommonJS format, but you can choose others like 'esm' for ES modules
+        //   sourcemap: true,
+        // },
+        {
+            file: 'dist/bundle.cjs.js',
+            format: 'cjs',
+            exports: 'named',
+            sourcemap: true,
+        },
+        {
+            file: 'dist/bundle.esm.js',
+            format: 'es',
+            sourcemap: true,
+        },
+    ],
+    plugins: [
+        // Resolve node_modules dependencies
+        nodeResolve({
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        }),
 
-    // Convert CommonJS modules to ES6
-    commonjs(),
+        // Convert CommonJS modules to ES6
+        commonjs(),
 
-    sass({ insert: true }), // Add the Sass plugin
+        sass({insert: false, output: 'dist/styles.css'}), // Add the Sass plugin
 
-    // Compile TypeScript to JavaScript
-    typescript({
-      tsconfig: 'tsconfig.json', // Path to your TypeScript configuration file
-    }),
+        // Compile TypeScript to JavaScript
+        // typescript({
+        //   tsconfig: 'tsconfig.json', // Path to your TypeScript configuration file
+        // }),
 
-    // Transpile JavaScript and JSX code using Babel
-    babel({
-      exclude: 'node_modules/**',
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-    }),
-  ],
-  external: ['react', 'react-dom'], // Specify external dependencies to prevent bundling them
+        // Transpile JavaScript and JSX code using Babel
+        babel({
+            exclude: 'node_modules/**',
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            babelHelpers: 'bundled',
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+        }),
+    ],
+    external: ['react', 'react-dom'], // Specify external dependencies to prevent bundling them
 };
