2dfd0ad5eef5961844065c435093e811d17ed683
jianlan <jianlan1991@gmail.com>
2024-03-28 19:43:46 +0800

    menu open and closing animation

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,7 +3,10 @@ import List from "../List/List";
 import {BaseProps} from "../internal/common/BaseProps";
 import MenuItem, {MenuItemHandle, MenuItemProps} from "./MenuItem";
 import {Corner} from "../internal/alignment/geometry";
+import {EASING} from "../internal/motion/animation";
+import c from 'classnames'
 import './Menu.scss'
+import Elevation from "../Elevation";
 
 export interface MenuProps extends BaseProps {
   items?: MenuItemProps[]
@@ -17,7 +20,7 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
   const {
     items,
     style,
-    open,
+    open: OPEN,
     anchorAlignCorner = Corner.END_START,
     menuAlignCorner = Corner.START_START,
     anchorEl,
@@ -26,7 +29,16 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
 
   const itemRefs = useRef<HTMLElement[]>([]);
   const menuRef = useRef<HTMLDivElement>(null);
-  const [menuOffsetStyle, setMenuOffsetStyle] = useState<CSSProperties>()
+  const listRef = useRef<HTMLUListElement>(null)
+
+  const rootHeightAnimation = useRef<Animation>();
+  const rootOpacityAnimation = useRef<Animation>();
+  const childrenAnimations = useRef<Animation[]>([])
+
+  const [menuOffsetStyle, setMenuOffsetStyle] = useState<CSSProperties>();
+  const [open, setOpen] = useState<boolean>(false);
+
+  const openDirection = menuAlignCorner.toString().startsWith('end') ? 'UP' : 'DOWN'
 
   const getChildren = () => {
     return items?.map((itemProps, index) => {
@@ -57,7 +69,6 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
     const {offsetLeft: anchorLeft, offsetTop: anchorTop} = anchor
     const {innerHeight, innerWidth} = window
 
-
     const anchorCorners = {
       [Corner.START_START]: {x: anchorLeft, y: anchorTop},
       [Corner.START_END]: {x: anchorLeft + anchorWidth, y: anchorTop},
@@ -67,8 +78,6 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
 
     let menuPosition: { top?: number, left?: number, bottom?: number } = {}
     const {y, x} = anchorCorners[anchorCorner]
-    console.log('anchor corner:', x, y)
-
     switch (menuCorner) {
       case Corner.START_START:
         menuPosition = {
@@ -95,7 +104,6 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
         }
         break
     }
-    console.log('menu position:', menuPosition)
 
     if (menuPosition.top && menuPosition.top + menuTop > innerHeight) {
       const offsetTop = menuTop + menuPosition.top
@@ -119,8 +127,92 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
     return menuPosition
   }
 
-  const animateOpen = () => {
+  const animateOpen = (rootEl: HTMLElement, list: HTMLElement) => {
+    const openingUpwards = openDirection === 'UP';
+    const FULL_DURATION = 500;
+    const OPACITY_DURATION = 50;
+    const ITEM_OPACITY_DURATION = 250;
+    const DELAY_BETWEEN_ITEMS = (FULL_DURATION - ITEM_OPACITY_DURATION) / itemRefs.current.length;
 
+    const {height,} = rootEl.getBoundingClientRect()
+
+    const rootHeightAnimation = rootEl.animate({
+      height: ['0', `${height}px`]
+    }, {duration: FULL_DURATION, easing: EASING.EMPHASIZED})
+
+    const rootOpacityAnimation = rootEl.animate({
+      opacity: ['0', '1'],
+    }, {duration: OPACITY_DURATION})
+
+    const upPositionCorrectionAnimation = list.animate([
+      {transform: openingUpwards ? `translateY(-${height}px)` : ''},
+      {transform: ''},
+    ], {duration: FULL_DURATION, easing: EASING.EMPHASIZED});
+
+    const childrenAnimations: Animation[] = []
+    for (let i = 0; i < itemRefs.current.length; i++) {
+      const directionalIndex = openingUpwards ? itemRefs.current.length - 1 - i : i;
+      const child = itemRefs.current[directionalIndex]
+      const childOpacityAnimation = child.animate({
+        opacity: ['0', '1'],
+      }, {duration: ITEM_OPACITY_DURATION, delay: DELAY_BETWEEN_ITEMS * i, fill: 'backwards'})
+      childrenAnimations.push(childOpacityAnimation)
+    }
+
+    Promise.all([rootHeightAnimation.finished, rootOpacityAnimation.finished, upPositionCorrectionAnimation.finished]).then(() => {
+      rootHeightAnimation.cancel()
+      rootOpacityAnimation.cancel()
+      childrenAnimations.forEach(childAnimation => {
+        childAnimation.cancel()
+      })
+    })
+  }
+
+  const animateClose = (rootEl: HTMLElement, listEl: HTMLElement) => {
+    const closingDownwards = openDirection === 'UP';
+    const FULL_DURATION = 150;
+    const OPACITY_DURATION = 50;
+    const OPACITY_DELAY = FULL_DURATION - OPACITY_DURATION
+    const ITEM_OPACITY_DURATION = 50;
+    const ITEM_OPACITY_INITIAL_DELAY = 50;
+    const END_HEIGHT_PERCENTAGE = 0.35;
+    const DELAY_BETWEEN_ITEMS = (FULL_DURATION - ITEM_OPACITY_INITIAL_DELAY - ITEM_OPACITY_DURATION) / itemRefs.current.length;
+
+    const {height,} = rootEl.getBoundingClientRect()
+
+    rootHeightAnimation.current = rootEl.animate({
+      height: [`${height}px`, `${height * END_HEIGHT_PERCENTAGE}px`]
+    }, {duration: FULL_DURATION, easing: EASING.EMPHASIZED_ACCELERATE, fill: 'forwards'})
+
+    rootOpacityAnimation.current = rootEl.animate({
+      opacity: ['1', '0']
+    }, {duration: OPACITY_DURATION, delay: OPACITY_DELAY, fill: 'forwards'})
+
+    const downPositionCorrectionAnimation = listEl.animate([
+      {transform: ''},
+      {
+        transform: closingDownwards
+          ? `translateY(-${height * (1 - END_HEIGHT_PERCENTAGE)}px)`
+          : '',
+      },
+    ], {duration: FULL_DURATION, easing: EASING.EMPHASIZED_ACCELERATE});
+
+    for (let i = 0; i < itemRefs.current.length; i++) {
+      const directionalIndex = closingDownwards ? i : itemRefs.current.length - 1 - i;
+      const child = itemRefs.current[directionalIndex];
+      const childOpacityAnimation = child.animate({
+        opacity: ['1', '0'],
+      }, {
+        duration: ITEM_OPACITY_DURATION,
+        delay: ITEM_OPACITY_INITIAL_DELAY + DELAY_BETWEEN_ITEMS * i,
+        fill: 'forwards'
+      })
+      childrenAnimations.current.push(childOpacityAnimation)
+    }
+
+    Promise.all([rootHeightAnimation.current.finished, rootOpacityAnimation.current.finished, downPositionCorrectionAnimation.finished]).then(() => {
+      setOpen(false)
+    })
   }
 
   useEffect(() => {
@@ -137,18 +229,35 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
   }, [menuRef, anchorEl, anchorAlignCorner, menuAlignCorner]);
 
   useEffect(() => {
-    if (open) {
-      animateOpen()
-    } else if (open === false) {
-
+    if (OPEN) {
+      !open && setOpen(true)
     } else {
+      open && menuRef.current && listRef.current && animateClose(menuRef.current, listRef.current)
+    }
+  }, [OPEN]);
 
+  useEffect(() => {
+    if (open) {
+      menuRef.current && listRef.current && animateOpen(menuRef.current, listRef.current)
+    } else {
+      rootHeightAnimation.current?.cancel()
+      rootOpacityAnimation.current?.cancel()
+      childrenAnimations.current.forEach(childAnimation => {
+        childAnimation.cancel()
+      })
     }
   }, [open]);
 
   return (
-    <div ref={menuRef} style={{...style, ...menuOffsetStyle}} className={'menu'}>
-      <List {...rest}>
+    <div
+      ref={menuRef}
+      style={{...style, ...menuOffsetStyle}}
+      className={c('menu', {
+        'open': open
+      })}
+    >
+      <Elevation></Elevation>
+      <List ref={listRef} {...rest}>
         {getChildren()}
       </List>
     </div>
