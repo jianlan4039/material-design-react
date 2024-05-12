b3d3c05d88fd73157e0e7e80c0be5ff4bf47c559
jianlan <jianlan1991@gmail.com>
2024-05-12 14:22:36 +0800

    clock

---
+++ src/components/TimePicker/TimePicker.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export interface ITimePickerProps {
+
+}
+
+const TimePicker:React.FC<ITimePickerProps> = (props) => {
+
+  return (
+    <div className={'time-picker'}>
+
+    </div>
+  )
+}
+
+export default TimePicker
---
+++ src/components/TimePicker/internal/Clock.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Clock.scss';
+
+export interface IClockProps {
+
+}
+
+const Clock: React.FC<IClockProps> = (props) => {
+
+  return (
+    <div className={'clock'}>
+
+    </div>
+  )
+}
+
+export default Clock
