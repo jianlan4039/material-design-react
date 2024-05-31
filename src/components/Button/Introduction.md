9f8e9a9c0ed7c598f73e386673921cbc59839a49
jianlan <jianlan1991@gmail.com>
2024-05-31 11:23:21 +0800

    button icon issues

---
+++ src/components/Button/Introduction.md
@@ -0,0 +1,2 @@
+# Common Button
+There are 5 kinds of common buttons, 
--- src/stories/ElevatedButton.stories.tsx
+++ src/stories/ElevatedButton.stories.tsx
@@ -1,6 +1,7 @@
 import {Meta, StoryObj} from "@storybook/react";
 import React from "react";
 import ElevatedButton from "../components/Button/ElevatedButton";
+import {FilledIcon} from "../icons";
 
 const meta: Meta<typeof ElevatedButton> = {
   component: ElevatedButton,
@@ -14,8 +15,30 @@ const meta: Meta<typeof ElevatedButton> = {
 export default meta;
 type Story = StoryObj<typeof ElevatedButton>;
 
-export const Elevated: Story = {
+export const Default: Story = {
   render: (args) => (
     <ElevatedButton {...args}>Elevated</ElevatedButton>
   )
+}
+
+export const WithSvgIcon: Story = {
+  render: () => {
+    const Icon = (
+      <FilledIcon>home</FilledIcon>
+    )
+
+    const TrailIcon = (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+        <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
+      </svg>
+    )
+
+    return (
+      <ElevatedButton
+        icon={Icon}
+        // trailingIcon={TrailIcon}
+      >
+        Elevated
+      </ElevatedButton>)
+  }
 }
