9fd4b4ce85f724603c42182785fc28c86dfa3cca
jianlan <jianlan1991@gmail.com>
2024-06-16 10:14:58 +0800

    1.change checkbox to use useRipple and useFocusRing hooks

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,12 +1,14 @@
-import React, {forwardRef, ReactNode, useEffect, useState} from 'react'
+import React, {forwardRef, ReactNode, useEffect, useRef, useState} from 'react'
 import CheckboxContent, {CheckboxContentProps, setState as _setState} from "./internal/CheckboxContent";
 import './Checkbox.scss'
 import withStateLayer from "../StateLayer";
 import cln from "classnames";
 import {StateElement} from "../internal/common/StateElement";
 import withFocusRing, {FocusRingProps} from "../Focus";
+import useRipple from "../Ripple/useRipple";
+import useFocusRing from "../Focus/useFocusRing";
 
-export interface CheckboxProps extends CheckboxContentProps, StateElement, FocusRingProps {
+export interface CheckboxProps extends CheckboxContentProps {
   children?: ReactNode
 }
 
@@ -17,12 +19,23 @@ const Checkbox = withFocusRing(withStateLayer(forwardRef<HTMLInputElement, Check
     indeterminate: _indeterm = false,
     disabled,
     error,
-    stateLayer,
-    focusRing,
+    onBlur,
+    onFocus,
     ...rest
   } = props
 
   const [state, setState] = useState<number>(_indeterm ? 2 : _chk ? 1 : 0)
+  const checkbox = useRef<HTMLInputElement>(null);
+  const [parent, setParent] = useState<HTMLInputElement>()
+
+  const [rippleProps, ripple] = useRipple<HTMLDivElement>({})
+  const [focusRingProps, focusRing] = useFocusRing<HTMLInputElement>({parent, onFocus, onBlur})
+
+  useEffect(() => {
+    if (checkbox.current) {
+      setParent(checkbox.current)
+    }
+  }, [checkbox]);
 
   const clickHandler = () => {
     if (state === 2) {
@@ -46,14 +59,16 @@ const Checkbox = withFocusRing(withStateLayer(forwardRef<HTMLInputElement, Check
         'nd-error': error
       })}
       onClick={clickHandler}
+      {...rippleProps}
     >
+      {ripple}
       {focusRing}
-      {!disabled && stateLayer}
       <CheckboxContent
-        ref={ref}
+        ref={checkbox}
         disabled={disabled}
         checked={state === 1}
         indeterminate={state === 2}
+        {...focusRingProps}
         {...rest}
       ></CheckboxContent>
     </div>
