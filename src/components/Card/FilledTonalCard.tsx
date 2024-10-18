05775dd56c117c28aa578bfd3fae52f2fef56cc4
jianlan4039 <jianlan1991@gmail.com>
2024-10-18 23:21:42 +0800

    1.添加FilledTonalCard

---
+++ src/components/Card/FilledTonalCard.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import {FilledCardProps} from "./FilledCard";
+
+export interface FilledTonalCardProps {
+
+}
+
+
+const FilledTonalCard = (props: FilledCardProps) => {
+
+
+  return <div>
+
+  </div>
+}
