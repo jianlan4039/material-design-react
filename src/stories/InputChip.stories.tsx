99d27fe9696766e60b143e97abfb2a359843b5d9
jianlan4039 <jianlan1991@gmail.com>
2024-11-12 16:47:47 +0800

    1.alter input chip scss token namespace; 2.add input chip stories;

---
+++ src/stories/InputChip.stories.tsx
@@ -0,0 +1,20 @@
+import {Meta, StoryObj} from '@storybook/react';
+import './styles'
+import {InputChip} from "../index";
+
+const meta: Meta = {
+  title: 'Chip/Input Chip',
+  component: InputChip,
+  tags: ['autodocs']
+}
+
+export default meta;
+type Story = StoryObj<typeof InputChip>;
+
+export const Primary: Story = {
+  args: {
+    label: 'Filter Chip',
+    disabled: false,
+    icon: <span className={"material-icons-round"}>home</span>
+  }
+}
