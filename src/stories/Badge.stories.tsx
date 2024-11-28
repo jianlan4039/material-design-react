a634209394f042040cb5a4a0582f9a03cdcb1a79
jianlan4039 <jianlan1991@gmail.com>
2024-11-28 09:43:01 +0800

    1. alter badge inset inline start.

--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -11,11 +11,33 @@ export default meta;
 
 type Story = StoryObj<typeof Badge>;
 
-export const Primary: Story = {
+export const Small: Story = {
   render: () => {
     return (
       <Badge count={5}>
         <span className={'material-icons'}>home</span>
+      </Badge>
+    )
+  }
+}
+
+export const Large: Story = {
+  render: ({count = 10, size = "large"}) => {
+    return (
+      <Badge count={count} size={size}>
+        <span className="material-icons">home</span>
+      </Badge>
+    )
+  }
+}
+
+export const withIconButton: Story = {
+  render: ({count = 10, size = "large"}) => {
+    return (
+      <Badge count={count} size={size}>
+        <IconButton>
+          <span className={'material-icons'}>home</span>
+        </IconButton>
       </Badge>
     )
   }
