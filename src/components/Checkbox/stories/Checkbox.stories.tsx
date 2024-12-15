a9aa08ae3d6dd7ce54858f30dca47f21e1c1ea11
jianlan4039 <jianlan1991@gmail.com>
2024-12-15 17:01:13 +0800

    1. move checkbox stories to checkbox component directory

--- src/components/Checkbox/stories/Checkbox.stories.tsx
+++ src/components/Checkbox/stories/Checkbox.stories.tsx
@@ -1,6 +1,5 @@
 import {Meta, StoryObj} from '@storybook/react';
-import '../styles';
-import {Checkbox} from "../../index";
+import {Checkbox} from "../../../index";
 
 const meta: Meta = {
   title: 'Form/Checkbox',
